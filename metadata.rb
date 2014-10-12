6d6171fb3b1ad3b53fa8abb2ad604f7cb2719582
heathsnow <heath.snow@changepoint.com>
2014-10-11 18:10:35 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'dl_teamengineering@daptiv.com'
 license          'All rights reserved'
 description      'A collection of resources for managing Citrix NetScaler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+version          '0.0.6'
