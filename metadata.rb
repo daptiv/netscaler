75426b43d02172759a545382c061fbde2c28554e
heathsnow <heath.snow@changepoint.com>
2017-10-20 15:16:23 -0700

    changelog update

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'A collection of resources for managing Citrix NetScaler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.4'
+issues_url 'https://github.com/daptiv/netscaler/issues'
+source_url 'https://github.com/daptiv/netscaler/'
